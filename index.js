94f1b9f245e44d32bf71bd393fe7a4ddb900824a
abuishak <39011865+abuishak@users.noreply.github.com>
2020-08-22 13:45:18 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+  const number =$("#number-of-dog").val()
+  fetch(`https://dog.ceo/api/breeds/image/random/${number}`)
+
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
@@ -10,12 +12,16 @@ function getDogImage() {
 
 function displayResults(responseJson) {
   console.log(responseJson);
-  //replace the existing image with the new one
-  $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img">`
-  )
-  //display the results section
-  $('.results').removeClass('hidden');
+  const imageUrls = responseJson.message
+  
+
+const myImageTags = imageUrls.map(url => "<img src='" + url + "'class='.results-img'/>")
+
+//console.log(myImageTags)
+$('.results-img').remove()
+$('.results').append(myImageTags)
+ 
+ $('.results').removeClass('hidden');
 }
 
 function watchForm() {
